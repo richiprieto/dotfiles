63561e3150a997b0a78a7f374f1ccbbb479dd8ae
Ricardo M. Prieto <richiprietog@gmail.com>
2024-05-09 11:52:06 -0500

    configuraciones recientes

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,5 +7,9 @@ local config = wezterm.config_builder()
 -- Font
 -- config.font = wezterm.font("Hack Nerd Font", { weight = "Regular", stretch = "Normal", style = "Normal" })
 config.font = wezterm.font("MesloLGS NF")
+config.font_size = 10.0
+
+-- buffer size
+config.scrollback_lines = 10000
 
 return config
---
+++ copy.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/bash
+#
+cp -r ~/.config/nvim/ .
+cp ~/.p10k.zsh .
+cp ~/.wezterm.lua .
+cp ~/.zshrc .
